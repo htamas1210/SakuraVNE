d20361b4e89327e8039472ac59e1ef6e999856ab
Hatvani Tamás <hatvanitamas09@gmail.com>
2026-04-16 13:49:29 +0200

    new core version, window flags

--- SakuraVNE/src/main.cpp
+++ SakuraVNE/src/main.cpp
@@ -2,10 +2,12 @@
 #include "Application.h"
 #include "Overlay.h"
 #include "SDL3/SDL_main.h"
+#include "SDL3/SDL_video.h"
 
 int main(int argc, char *argv[]) {
     SakuraVNE::AppData appData;
     appData.name = "Sakura Visual Novel Creator";
+    appData.windowdata.windowFlags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY;
     appData.windowdata.title = appData.name;
     appData.windowdata.width = 1280;
     appData.windowdata.height = 720;
